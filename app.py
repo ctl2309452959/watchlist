1cd21871b70e204e3bc8f39723a77244ae7ca997
smart <2309452959@qq.com>
2020-02-25 15:11:00 +0800

    添加表单

--- app.py
+++ app.py
@@ -1,56 +1,97 @@
 import os
 import sys
+import click
 
-from flask import Flask
-from flask import render_template
+from flask import Flask,render_template,request,url_for,redirect,flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'Bruce'
 
 WIN = sys.platform.startswith('win')
 if WIN:
-    prefix = 'sqlite:///'  # 如果是windows操作系统，是三个斜杠
+    prefix = 'sqlite:///'  # 如果是windows系统，三个斜杠
 else:
-    prefix = 'sqlite:////' # 如果是Mac，Linux，是四个斜杠
+    prefix = 'sqlite:////'  # Mac，Linux，四个斜杠
 
 # 配置
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
 
+db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    name = "Smart"
+# 创建数据库模型类
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True) # 主键
+    name = db.Column(db.String(20)) 
+    
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key=True) # 主键
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+# 自定义initdb
+@app.cli.command()
+@click.option('--drop',is_flag=True,help='删除之后再创建')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('初始化数据库')
+
+# 自定义命令forge，把数据写入数据库
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = "Bruce"
     movies = [
-        {"title":"杀破狼", "year":"2003"},
-        {"title":"扫毒", "year":"2018"},
-        {"title":"捉妖记", "year":"2016"},
-        {"title":"囧妈", "year":"2020"},
-        {"title":"葫芦娃", "year":"1989"},
-        {"title":"玻璃盒子", "year":"2020"},
-        {"title":"调酒师", "year":"2020"},
-        {"title":"釜山行", "year":"2017"},
-        {"title":"导火索", "year":"2005"},
-        {"title":"叶问", "year":"2015"},
-
+        {'title':'杀破狼','year':'2003'},
+        {'title':'扫毒','year':'2018'},
+        {'title':'捉妖记','year':'2016'},
+        {'title':'囧妈','year':'2020'},
+        {'title':'葫芦娃','year':'1989'},
+        {'title':'玻璃盒子','year':'2020'},
+        {'title':'调酒师','year':'2020'},
+        {'title':'釜山行','year':'2017'},
+        {'title':'导火索','year':'2005'},
+        {'title':'叶问','year':'2015'}
     ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('数据导入完成')
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        # 获取表单的数据
+        title = request.form.get('title')
+        year = request.form.get('year')
 
-    return render_template('index.html',name = name , movies = movies)
+        # 验证title，year不为空，并且title长度不大于60，year的长度不大于4
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('输入错误')  # 错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+        
+        movie = Movie(title=title,year=year)  # 创建记录
+        db.session.add(movie)  # 添加到数据库会话
+        db.session.commit()   # 提交数据库会话
+        flash('数据创建成功')
+        return redirect(url_for('index'))
 
+    movies = Movie.query.all()
+    return render_template('index.html',movies=movies)
 
+@app.errorhandler(404) # 传入要处理的错误代码
+def page_not_found(e):
+    return render_template('404.html'),404
 
+@app.context_processor # 模板上下文处理函数
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.run(host="127.0.0.1", port="3000")
+if __name__ == "__main__":
+    app.run()
